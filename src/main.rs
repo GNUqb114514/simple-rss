cdf90c73351bb4688eaf8f205c2ccfa7f1881889
qb114514 <phao0724@163.com>
2024-08-03 09:25:38 +0800

    docs(docstring): Add docstrings for struct crates::Cli & crates::Command

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
+/// An easy-to-use RSS reader.
 struct Cli {
     command: Option<Command>,
 }
 
 enum Command {
+    /// Get registered feeds.
     GetFeeds {},
+    /// Get unread articles from a feed / all feeds.
     GetUnread {feed: Option<String>},
+    /// Get raw article content.
     GetRaw {feed: String, id: u64},
+    /// Get HTML content of an article.
     GetHtml {feed: String, id: u64},
+    /// Render an article into the terminal.
     Render {feed: String, id: u64},
+    /// Read articles in the TUI.
     TuiRead {},
 }
 
