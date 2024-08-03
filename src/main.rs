5230c435caa812876e4d36207d45d90cefda3304
qb114514 <phao0724@163.com>
2024-08-03 09:18:22 +0800

    feat(cli): Add structs for cli command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+struct Cli {
+    command: Option<Command>,
+}
+
+enum Command {
+    GetFeeds {},
+    GetUnread {feed: Option<String>},
+    GetRaw {feed: String, id: u64},
+    GetHtml {feed: String, id: u64},
+    Render {feed: String, id: u64},
+    TuiRead {},
+}
+
 fn main() {
     println!("Hello, world!");
 }
